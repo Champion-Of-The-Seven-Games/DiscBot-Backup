1d0025dcd9e7fde3726b7a525ed6ac01d43e09fa
Champion Of The Seven Games <>
2021-04-20 07:46:54 +0000

    defined the latency command in more detail

--- commands/misc/latency.js
+++ commands/misc/latency.js
@@ -5,6 +5,24 @@ module.exports = {
     message.channel.send('calculating the latency...').then((resultMessage) => {
       const botLatency = resultMessage.createdTimestamp - message.createdTimestamp
       resultMessage.edit(`Bot latency: ${botLatency}ms`)
+      if (botLatency <= 100) {
+        resultMessage.edit(`
+Bot latency: ${botLatency}ms
+the latency is lower than usual (very good)
+        `)
+      }
+      if (botLatency > 100 && botLatency < 200) {
+      resultMessage.edit(`
+Bot latency: ${botLatency}ms
+the latency is normal (good)
+        `)
+      }
+      if (botLatency >= 200) {
+      resultMessage.edit(`
+Bot latency: ${botLatency}ms
+the latency is higher than usual (bad)
+        `)
+      }
     })
   },
 }
