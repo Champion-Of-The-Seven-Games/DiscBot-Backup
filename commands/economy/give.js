e3419817e21431f93d778b7910acd367f99b811a
Champion Of The Seven Games <>
2021-05-09 10:03:02 +0000

    updated the give command

--- commands/economy/give.js
+++ commands/economy/give.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 const economy = require('../../command-supporters/economy')
 
 module.exports = {
@@ -8,6 +9,7 @@ module.exports = {
   maxArgs: 2,
   callback: async (message, arguments) => {
     const {member} = message
+    const data = []
 
     if (message.mentions.users.first()) {
       const target = message.mentions.users.first()
@@ -20,20 +22,59 @@ module.exports = {
           return
         }
 
-        const remainingCoins = await economy.addCoins(
-          member.id,
-          coinsToGive * -1
-        )
-        const newBalalnce = await economy.addCoins(
-          target.id,
-          coinsToGive
-        )
+        const questions = [
+          'Are you sure? type yes (case sensitive) in the chat within the next 10 seconds'
+        ]
+        let qcounter = 0
 
-        message.reply(`you have given ${coinsToGive} coins to <@${target.id}>, they now have ${newBalalnce} and you have ${remainingCoins} coins`)
+        const filter = (m) => {
+          return m.author.id === message.author.id
+        }
 
-        try {
-          target.send(`You have been given ${coinsToGive} coins by ${message.author.username} in ${message.guild.name}, you now have ${newBalalnce} coins`)
-        }catch{}
+        const collector = new Discord.MessageCollector(message.channel, filter, {
+          max: questions.length,
+          time: 1000 * 10,
+        })
+
+        message.channel.send(questions[qcounter++])
+        collector.on('collect', (m) => {
+          if (qcounter < questions.length) {
+            m.channel.send(questions[qcounter++])
+          }
+        })
+
+        collector.on('end', (collected) => {
+          if (collected.size < questions.length) {
+            message.reply('cancelled the transaction')
+            return
+          }
+
+          collected.forEach((value) => {
+            data.push(value.content)
+          })
+        })
+
+        setTimeout( async () => {
+          if (data[0] === 'yes') {
+            const remainingCoins = await economy.addCoins(
+              member.id,
+              coinsToGive * -1
+            )
+            const newBalalnce = await economy.addCoins(
+              target.id,
+              coinsToGive
+            )
+
+            message.reply(`you have given ${coinsToGive} coins to <@${target.id}>, they now have ${newBalalnce} and you have ${remainingCoins} coins`)
+
+            try {
+              target.send(`You have been given ${coinsToGive} coins by ${message.author.username} in ${message.guild.name}, you now have ${newBalalnce} coins`)
+            }catch{}
+          }
+          else {
+            message.reply('cancelled the transaction')
+          }
+        }, 1000 * 10)
       }
       else if (isNaN(coinsToGive)) {
         message.reply('please provide a valid number of coins to transfer')
